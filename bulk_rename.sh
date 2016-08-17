30db977ddbd86f0b618c9234de4a0586366aa32b
Thomas Malcher <malcher@student.tugraz.at>
2016-08-17 14:03:24 +0200

    add initial version of a very simple but easy to use bulk rename script
    still needs some testing

---
+++ bulk_rename.sh
@@ -0,0 +1,84 @@
+#!/bin/sh
+
+#This script is thought to be used as very simple bulk renamer for nemo 
+#  go to Edit->Preferences->Behavior
+#  under "Command to invoke when renaming multiple items:" enter "bulk_rename.sh %F" 
+#
+#What it does: 
+# takes a list of filenames as input, 
+# asks the user for a new name
+# and renames all given files to the schema <new name>(<counter>).<original ending> 
+
+
+dry=0 #if set to 1 no rename operations are executed but all commands are shown as text
+dryres=""
+err_src_does_not_exist=""
+err_dst_already_exist=""
+err_could_not_move=""
+
+bname=$(zenity --entry --title="enter new base name")
+if [ $? -ne 0 ]; then
+    exit 0
+fi
+
+i=0
+for arg in "$@"
+do
+    # nemo replaces blanks in filenames with 0s, this is a bug. 
+    #https://github.com/linuxmint/nemo/issues/614
+    # for now it is not possible to rename file containing blanks
+
+    name="${arg#file://*}"
+    dirname=$(dirname "$name")
+    if [ -d "$name" ]; then
+        ending=""
+    elif [ -f "$name" -o -L "$name" ]; then
+        ending=".${name##*.}"
+        if [ "$ending" = ".$name" ]; then
+            ending=""
+        fi
+    else
+        err_src_does_not_exist="${err_src_does_not_exist}$name\n"
+        continue
+    fi
+    i=$((i+1))
+    newname="$dirname/$bname($i)$ending"
+    if [ -e "$newname" -o -L "$newname" ]; then
+        err_dst_already_exist= "${err_dst_already_exist}could not rename $name to $newname\n"
+        continue
+    fi
+    if [ $dry -eq 1 ]; then
+        dryres=$dryres"mv -n -T "$name" "$newname"\n"
+    else
+        mv -n -T "$name" "$newname" || err_could_not_move="${err_could_not_move}$name to $newname\n"
+    fi
+done
+
+err_msg=""
+if [ -n "$err_src_does_not_exist" ]; then
+    err_msg="${err_msg}Source does not exist:\n"
+    err_msg="$err_msg $err_src_does_not_exist"
+fi
+if [ -n "$err_dst_already_exist" ]; then
+    err_msg="${err_msg}File already exists:\n"
+    err_msg="$err_msg $err_dst_already_exist"
+fi
+if [ -n "$err_could_not_move" ]; then
+    err_msg="${err_msg}Could not rename:\n"
+    err_msg="$err_msg $err_could_not_move"
+fi
+if [ -n "$err_msg" ]; then
+    zenity --error --text "$err_msg"
+fi
+
+if [ $dry -eq 1 ]; then
+    zenity --info --text "$dryres"
+fi
+
+
+
+
+
+
+
+
